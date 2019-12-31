09fa0041ca010b8640b329392cf063e5908f835f
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2019-12-31 12:31:26 -0300

    First commit

---
+++ client/src/script.js
@@ -0,0 +1,46 @@
+const fetch = () => {
+  axios({
+      url: 'http://localhost:4000',
+      method: 'POST',
+      data: {
+          query: `
+            {
+                prefixes {
+                    id
+                    type
+                    description
+                }
+                sufixes {
+                    description
+                }
+            }
+          `
+      }
+  }).then(response => {
+      const query = response.data
+
+      const prefixes = query.data.prefixes
+      const sufixes = query.data.sufixes
+
+      console.log(prefixes)
+      console.log(sufixes)
+
+      const createItems = (items) => items.map(item => {
+        const li = document.createElement('li')
+        li.textContent = JSON.stringify(item)
+        return li
+      })
+
+      const appendDOM = (el, children) => {
+        const ul = document.querySelector(el)
+
+        children.map(child => ul.appendChild(child))
+      }
+
+      appendDOM('ul#prefixes', createItems(prefixes))
+      appendDOM('ul#sufixes', createItems(sufixes))
+
+  })
+};
+
+fetch();
