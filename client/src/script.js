d3c2650b3df659d7e6747e45bca045612abc845c
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2019-12-31 19:45:40 -0300

    Adding mutation

--- client/src/script.js
+++ client/src/script.js
@@ -1,46 +1,111 @@
-const fetch = () => {
-  axios({
-      url: 'http://localhost:4000',
-      method: 'POST',
-      data: {
+const Item = {
+  all(fn) {
+    axios({
+        url: 'http://localhost:4000',
+        method: 'POST',
+        data: {
+            query: `
+              {
+                  prefixes: items (type: "prefix") {
+                      id
+                      type
+                      description
+                  }
+                  sufixes: items (type: "sufix") {
+                      description
+                  }
+              }
+            `
+        }
+    }).then(response => fn(response))
+  },
+
+  save(type, description , fn) {
+    if (description) {
+      axios({
+        url: 'http://localhost:4000',
+        method: 'POST',
+        data: {
           query: `
-            {
-                prefixes: items (type: "prefix") {
-                    id
-                    type
-                    description
-                }
-                sufixes: items (type: "sufix") {
-                    description
-                }
+            mutation ($item: ItemInput) {
+              newPrefix: saveItem (item: $item) {
+                id
+                type
+                description
+              }
             }
-          `
-      }
-  }).then(response => {
-      const query = response.data
-
-      const prefixes = query.data.prefixes
-      const sufixes = query.data.sufixes
-
-      console.log(prefixes)
-      console.log(sufixes)
-
-      const createItems = (items) => items.map(item => {
-        const li = document.createElement('li')
-        li.textContent = JSON.stringify(item)
-        return li
+          `,
+          variables: {
+            item: {
+              type,
+              description
+            }
+          }
+        }
       })
 
-      const appendDOM = (el, children) => {
-        const ul = document.querySelector(el)
+      fn()
+    }
+  },
 
-        children.map(child => ul.appendChild(child))
-      }
+  // delete(id, fn) {
+  //   axios({
+  //     url: 'http:localhost:4000',
+  //     method: 'POST',
+  //     data: {
+  //       query: `
+  //         mutation ($id: Int) {
+  //           deleted: deleteItem (id: $id)
+  //         }
+  //       `,
+  //       variables: {
+  //         id
+  //       }
+  //     }
+  //   }).then(() => fn())
+  // }
+}
 
-      appendDOM('ul#prefixes', createItems(prefixes))
-      appendDOM('ul#sufixes', createItems(sufixes))
+const appendChildren = (response) => {
+    const query = response.data
+    const prefixes = query.data.prefixes
+    const sufixes = query.data.sufixes
 
+    console.clear()
+    console.log(prefixes)
+    console.log(sufixes)
+
+    const createChildren = (items) => items.map(item => {
+      const li = document.createElement('li')
+      li.textContent = JSON.stringify(item)
+      return li
+    })
+
+    const appendDOM = (el, children) => {
+      const ul = document.querySelector(el)
+      ul.innerHTML = ''
+
+      children.map(child => ul.appendChild(child))
+    }
+
+    appendDOM('ul#prefixes', createChildren(prefixes))
+    appendDOM('ul#sufixes', createChildren(sufixes))
+}
+
+Item.all(appendChildren)
+
+const form = document.querySelector('form')
+form.addEventListener('submit', event => event.preventDefault())
+
+const addEventClickToButton = (btn, input, type) => {
+  const button = document.querySelector(btn)
+
+  button.addEventListener('click', () => {
+    const val = document.querySelector(input).value
+
+    Item.save(type, val, () => Item.all(appendChildren))
   })
-};
+}
 
-fetch();
+addEventClickToButton('button#prefixes', 'input#prefixes', 'prefix')
+addEventClickToButton('button#sufixes', 'input#sufixes', 'sufix')
